513167f87c3e8aa99f6522353cffee70fdfd6c48
Guilherme Ribeiro <48655138+guilhermelRibeiroo@users.noreply.github.com>
2019-06-18 14:40:09 -0300

    Adicionado Repositório, Model e View com Editar, Cadastrar e Editar para Pessoas Fisicas

---
+++ Model/PessoaFisica.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PessoaFisica
+    {
+        public int Id;
+        public string Nome;
+        public DateTime DataNascimento;
+        public string RG;
+        public string CPF;
+        public string Sexo;
+    }
+}
---
+++ View/Controllers/ClientePessoaFisicaController.cs
@@ -0,0 +1,69 @@
+﻿using Model;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class ClientePessoaFisicaController : Controller
+    {
+        // GET: ClientePessoaFisica
+        public ActionResult Index(string busca)
+        {
+            PessoaFisicaRepository repositorio = new PessoaFisicaRepository();
+            ViewBag.ClientesPessoaFisica = repositorio.ObterTodos(busca);
+
+            return View();
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome, string cpf, string rg, string dataNascimento, string sexo)
+        {
+            PessoaFisicaRepository repositorio = new PessoaFisicaRepository();
+            PessoaFisica pessoa = new PessoaFisica();
+            pessoa.Nome = nome;
+            pessoa.CPF = cpf;
+            pessoa.RG = rg;
+            pessoa.DataNascimento = Convert.ToDateTime(dataNascimento);
+            pessoa.Sexo = sexo;
+            repositorio.Inserir(pessoa);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            PessoaFisicaRepository repositorio = new PessoaFisicaRepository();
+            repositorio.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            PessoaFisicaRepository repositorio = new PessoaFisicaRepository();
+            PessoaFisica pessoa = repositorio.ObterPeloId(id);
+            ViewBag.PessoaFisicaEditar = pessoa;
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome, string cpf, string rg, string dataNascimento, string sexo)
+        {
+            PessoaFisicaRepository repositorio = new PessoaFisicaRepository();
+            PessoaFisica pessoa = new PessoaFisica();
+            pessoa.Id = id;
+            pessoa.Nome = nome;
+            pessoa.CPF = cpf;
+            pessoa.RG = rg;
+            pessoa.DataNascimento = Convert.ToDateTime(dataNascimento);
+            pessoa.Sexo = sexo;
+            repositorio.Atualizar(pessoa);
+            return RedirectToAction("Index");
+        }
+    }
+}
